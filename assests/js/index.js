908ef90c8fd025c1a676ee200c182751d6636bff
Gabriel Parrini <gabriel.parrini@gmail.com>
2022-04-08 16:15:02 -0400

    Viajes de Chile

---
+++ assests/js/index.js
@@ -0,0 +1,12 @@
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+$(document).ready(function () {
+  $("#boton").click(function () {
+    alert("El correo fue enviado correctamente...");
+  });
+});
